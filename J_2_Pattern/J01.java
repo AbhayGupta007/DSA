48b68cef33312760da70703c326d3a4448771ed6
ABHAY GUPTA <112316101+AbhayGupta007@users.noreply.github.com>
2024-04-25 00:18:32 +0530

    Changes

--- J_2_Pattern/J01.java
+++ J_2_Pattern/J01.java
@@ -57,5 +57,16 @@ public class J01 {
             System.out.println("");
             s.close();
         }
+
+        // Another compatible approach.
+        // for (int i = 1; i <= 2 * n - 1; i++) 
+        // {
+        //     for (int j = 1; j <= 2 * n - 1; j++) 
+        //     {
+        //         int val = n - Math.min(Math.min(i, 2 * n - i), Math.min(j, 2 * n - j)) + 1;
+        //         System.out.print(val + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
